d6b363ecb4bcda29440f4fed2fbd24d2ee18b690
xukai <xukai@encootech.com>
2021-01-26 20:01:15 +0800

    feat: add server.js

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const route = require('koa-route');
+const appMidware = require("./utils/appMidware");
+const opts = {
+  errorEventName:'error',
+      logDirectory:'./logs', // NOTE: folder must exist and be writable...
+      fileNamePattern:'roll-<DATE>.log',
+      dateFormat:'YYYY.MM.DD'
+};
+const log = require('simple-node-logger').createRollingFileLogger( opts );
+const app = new Koa();
+app.listen(9000);
+app.use(route.get('/',appMidware));
+
+app.on('error', (err, ctx) => {
+  log.error('server error', err, ctx)
+});
